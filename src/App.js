552e306e5a989cd393d8e90bba1bd285c301180d
jealvarez123 <jealvarez123@gmail.com>
2018-05-08 11:40:16 -0500

    created a route from the card to the app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import Card from './components/card/card';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello World</h1>
+        <Card />
       </div>
     );
   }
---
+++ src/components/card/card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './card.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello from the card</h1>
+
+      </div>
+    )
+  }
+}
+
+export default Card;
